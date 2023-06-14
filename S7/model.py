3061f9da634564165e836e85826128b0786cbc12
Madhur-1 <64495917+Madhur-1@users.noreply.github.com>
2023-06-15 01:18:04 +0530

    Update model.py

--- S7/model.py
+++ S7/model.py
@@ -32,7 +32,7 @@ class Net(nn.Module):
             nn.ReLU(),
             nn.BatchNorm2d(10),
         )
-        self.avgpool = nn.AvgPool2d(7)
+        self.avgpool = nn.AvgPool2d(5)
         # self.fc1 = nn.Linear(32, 10)
 
     def forward(self, x):
