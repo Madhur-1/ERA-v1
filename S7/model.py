47e0ffcb7b10a3a68b242965e12d77d85ebce070
Madhur-1 <me18b059@smail.iitm.ac.in>
2023-06-15 00:47:52 +0530

    S7 init commit

---
+++ S7/model.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    # This defines the structure of the NN.
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 16, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 32, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=1, stride=2),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 16, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 16, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 32, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+        )
+        self.avgpool = nn.AvgPool2d(5)
+        self.fc1 = nn.Linear(32, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        # x = F.relu(F.max_pool2d(x, 2))
+        x = self.conv2(x)
+        x = self.avgpool(x)
+        x = x.view(-1, 32)
+        x = self.fc1(x)
+        return F.log_softmax(x, dim=1)
