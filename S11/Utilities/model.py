3cabe32bdc56650b466fb9c4fd99458f4c5bf529
Madhur-1 <me18b059@smail.iitm.ac.in>
2023-07-28 16:37:48 +0530

    com,mit

--- S11/Utilities/model.py
+++ S11/Utilities/model.py
@@ -10,7 +10,9 @@ import torch.nn.functional as F
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_channels, out_channels, stride=1) -> None:
+    def __init__(
+        self, in_channels, out_channels, stride=1, dropout_percentage=0
+    ) -> None:
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_channels,
@@ -21,6 +23,7 @@ class BasicBlock(nn.Module):
             bias=False,
         )
         self.bn1 = nn.BatchNorm2d(out_channels)
+        self.drop1 = nn.Dropout(dropout_percentage)
         self.conv2 = nn.Conv2d(
             out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False
         )
@@ -91,24 +94,45 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
     def __init__(
-        self, block: nn.Module, num_blocks: list[int], num_classes: int = 10
+        self,
+        block: nn.Module,
+        num_blocks: list[int],
+        num_classes: int = 10,
+        dropout_percentage=0.0,
     ) -> None:
         super(ResNet, self).__init__()
         self.in_channels = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(
+            block, 64, num_blocks[0], stride=1, dropout_percentage=dropout_percentage
+        )
+        self.layer2 = self._make_layer(
+            block, 128, num_blocks[1], stride=2, dropout_percentage=dropout_percentage
+        )
+        self.layer3 = self._make_layer(
+            block, 256, num_blocks[2], stride=2, dropout_percentage=dropout_percentage
+        )
+        self.layer4 = self._make_layer(
+            block, 512, num_blocks[3], stride=2, dropout_percentage=dropout_percentage
+        )
         self.linear = nn.Linear(512 * block.expansion, num_classes)
 
-    def _make_layer(self, block, out_channels, num_blocks, stride) -> nn.Sequential:
+    def _make_layer(
+        self, block, out_channels, num_blocks, stride, dropout_percentage=0.0
+    ) -> nn.Sequential:
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(BasicBlock(self.in_channels, out_channels, stride))
+            layers.append(
+                BasicBlock(
+                    self.in_channels,
+                    out_channels,
+                    stride,
+                    dropout_percentage=dropout_percentage,
+                )
+            )
             self.in_channels = out_channels * block.expansion
         return nn.Sequential(*layers)
 
